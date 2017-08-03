c20961d271a154f04825ab70009da3a4e44c3d2d
simonepri <simone.pri@hotmail.it>
2017-08-04 01:07:51 +0200

    Update implementation

--- index.js
+++ index.js
@@ -3,54 +3,47 @@
 const crypto = require('crypto');
 const Buffer = require('safe-buffer').Buffer;
 
-/**
- * Implements Brad Hill's Double HMAC pattern from
- * https://www.nccgroup.trust/us/about-us/newsroom-and-events/blog/2011/february/double-hmac-verification/.
- * In short, it's near-impossible to write a reliable constant-time compare in a
- * high level language like JS, because of the many layers that can optimize
- * away attempts at being constant time.
+const HMAC_ALGORITHM = 'sha256';
+const HMAC_KEY_LENGTH = 32;
 
- * Double HMAC avoids that problem by blinding the timing channel instead. After
- * running the inputs through a second round of HMAC, we are free to
- * short-circuit comparison, because the time it takes to reach the
- * short-circuit has no relation to the similarity between the guessed digest
- * and the correct one.
- *
- * @param  {string} a The first buffer.
- * @param  {string} b The second buffer.
- * @return {boolean} true if equals, false otherwise.
+/**
+ * @param  {buffer} a
+ * @param  {buffer} b
+ * @return {boolean}
  */
 function timingSafeEqual(a, b) {
-  const key = crypto.randomBytes(32);
-  const ah = new crypto.Hmac('sha256', key).update(a).digest();
-  const bh = new crypto.Hmac('sha256', key).update(b).digest();
-
-  // The second test, for a.equals(b), is just in case of the vanishingly small
-  // chance of a collision. It only fires if the digest comparison passes and so
-  // doesn't leak timing information.
-  return ah.equals(bh) && a.equals(b);
+  let c = 0;
+  const len = a.length;
+  for (let i = 0; i < len; i++) {
+    c |= a[i] ^ b[i];
+  }
+  return c === 0;
 }
 
 /**
- * Does a constant-time String comparison using crypto native timingSafeEqual
- * if available or the Brad Hill's Double HMAC pattern if not.
+ * Does a constant-time String comparison using the Brad Hill's Double HMAC
+ * pattern.
  *
  * @param  {string} a The first string.
  * @param  {string} b The second string.
  * @return {boolean} true if equals, false otherwise.
  */
+
 function timingSafeStringEqual(sa, sb) {
   const ba = Buffer.from(sa);
   const bb = Buffer.from(sb);
 
-  if (sa.length !== sb.length) {
-    return false;
-  }
+  const key = crypto.randomBytes(HMAC_KEY_LENGTH);
+  const ha = new crypto.Hmac(HMAC_ALGORITHM, key).update(ba).digest();
+  const hb = new crypto.Hmac(HMAC_ALGORITHM, key).update(bb).digest();
 
+  // The second test, for ba.equals(bb), is just in case of the vanishingly small
+  // chance of a collision. It only fires if the digest comparison passes and so
+  // doesn't leak timing information.
   if (crypto.timingSafeEqual) {
-    return crypto.timingSafeEqual(ba, bb);
+    return crypto.timingSafeEqual(ha, hb) && ba.equals(bb);
   }
-  return timingSafeEqual(ba, bb);
+  return timingSafeEqual(ha, hb) && ba.equals(bb);
 }
 
 module.exports = timingSafeStringEqual;
